dfcd51047b15991b539e5501d2678a3aeb3f0650
Brahiam Steven <brahiam-1222@hotmail.com>
2022-03-08 23:51:58 -0500

    Proyecto_Cambio 3

--- index.php
+++ index.php
@@ -3,29 +3,52 @@
   
 <nav class="navbar navbar-expand navbar-light bg-faded">
     <div class="container ">
-        <a class="navbar-brand" href="#"><img class="logo" src="img/papa-noel.png" alt=""></a>
-        <ul class="navbar-nav me-6 mt-2 mt-lg-0">
+        <article class="article-Logo">
+            <div class="imglogo"><img class="logo" src="img/papa-noel.png" alt=""></div>
+            <div class="nomlogo">SOCIEDAD</div>
+        </article>
+    <article>
+
+        <!-- <a class="navbar-brand" href="#"><img class="logo" src="img/papa-noel.png" alt=""></a> -->
+        <ul class="navbar-nav me-6 mt-2 mt-lg-0 me-sm-2">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Active <span class="visually-hidden">(current)</span></a>
+                <a class="nav-link" href="#">Inicio <span class="visually-hidden">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="">Digitar</a>
+                
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Consultar</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Sugerencias</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
                 <ul> <div class="dropdown-menu" aria-labelledby="dropdownId">
+
                     <li><a class="dropdown-item" href="registros/embolse.php">Action 1</a></li>
                     <li><a class="dropdown-item" href="registros/embolse.php">Action 2</a></li>
-                    </div>
-                </ul>
-            </li>
-        </ul>
-        
-        <form class="d-flex my-2 my-lg-0">
-            <button class="btn btn-outline-success my-2 my-sm-0 " type="button">PERFIL</button>
-        </form>
+                </div>
+                
+            </ul>
+        </li>
+    </ul>
+</article>
+
+    
+<article class="article-perfil">
+    
+    <form class="d-flex my-0 my-lg-0">
+        <button class="btn btn-outline-success mx-4 my-2 my-sm-1 " type="button"><img class="perfil" src="img/papa-noel.png" alt=""></button>
+    </form>
+</article>
     </div>
 </nav>
+<div class="ratio ratio-4x3">
+<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTqWxJkYUPU3QtmkAy7gSy2NzBLTDGKNDnRdQ43FLf5RuC-pV4yKyb423XQ46VLecFob1qsBMNiVzPK/pubhtml?widget=true&amp;headers=false"></iframe>
+</div>
 </body>
 
   <?php include("pie.php")?>
