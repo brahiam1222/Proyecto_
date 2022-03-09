b3fd942314555d16ccc56679287942cd5e7cd624
brahi <brahi@DESKTOP-8V1T4Q4>
2022-03-09 08:01:17 -0500

    Proyecto cambio 4 Correcciones

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         <!-- <a class="navbar-brand" href="#"><img class="logo" src="img/papa-noel.png" alt=""></a> -->
         <ul class="navbar-nav me-6 mt-2 mt-lg-0 me-sm-2">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Inicio <span class="visually-hidden">(current)</span></a>
+                <a class="nav-link" href="#">Inicio</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="">Digitar</a>
@@ -26,15 +26,15 @@
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                <ul> <div class="dropdown-menu" aria-labelledby="dropdownId">
+                <ul class="dropdown-menu" aria-labelledby="dropdownId">
 
                     <li><a class="dropdown-item" href="registros/embolse.php">Action 1</a></li>
                     <li><a class="dropdown-item" href="registros/embolse.php">Action 2</a></li>
-                </div>
+                
                 
             </ul>
         </li>
-    </ul>
+        
 </article>
 
     
@@ -49,7 +49,8 @@
 <div class="ratio ratio-4x3">
 <iframe width="402" height="346" frameborder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=7CBD4D7F63B723F9%212836&authkey=%21ADSVKS00Qqt_koo&em=2&AllowTyping=True&ActiveCell='Hoja%201'!A1&wdInConfigurator=True"></iframe>
 </div>
+<?php include("pie.php")?>
 </body>
 
-  <?php include("pie.php")?>
+ 
 </html>
