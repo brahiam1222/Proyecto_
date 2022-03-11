4334f192d65ac7d16d546bc57819e080966756ba
brahi <brahi@DESKTOP-8V1T4Q4>
2022-03-11 06:30:44 -0500

    Proyecto cambio 6

--- index.php
+++ index.php
@@ -37,7 +37,6 @@
         
 </article>
 
-    
 <article class="article-perfil">
     
     <form class="d-flex my-0 my-lg-0">
@@ -46,10 +45,101 @@
 </article>
 </div>
 </nav>
+<div class="container">
+
+    <div class="table-responsive">
+        <table class="table align-middle">
+            <thead>
+                <tr>
+                    <th scope="col">sem_cosecha</th>
+                    <th scope="col">fecha</th>
+                    <th scope="col">finca</th>
+                    <th scope="col">destino</th>
+                    <th scope="col">sem_embolse</th>
+                    <th scope="col">cinta</th>
+                    <th scope="col">edad</th>
+                    <th scope="col">lt_01</th>
+                    <th scope="col">lt_02</th>
+                   <!-- <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_02</th>-->
+                    <th scope="col">Total</th>
+                
+                
+                </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td scope="row"><input class="sm" type="number" name="year" value="55" id=""></select><select name="" id=""></select></td>
+                <td><input type="date" name="" id=""></td>
+                <td><select name="Finca" id="Finca"></select></td>
+            </tr>
+            <tr>
+                <td scope="row"></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+
+
+</div>
+</div>
+
+
+
 
 <article>
+    
+    
+    <form action="" method="post">
+        
+        <ul>
+            <li>
+
+                sem_cosecha
+                
+                <select name="" id=""></select>
+                
+            </li>
+            <li>
+
+                
+                Fecha
+                <input type="datetime-local" name="" id="">
+            </li>
 
 
+        </ul>
+
+        
+    
+    
+    
+    
+    
+    
+    </form>
 
 </article>
 <!-- <div class="ratio ratio-4x3">
