d26e2a239301a6b0dc4165154505b048b60b6f4c
brahi <brahi@DESKTOP-8V1T4Q4>
2022-03-15 21:14:49 -0500

    Proyecto Cambio 7

--- index.php
+++ index.php
@@ -25,16 +25,23 @@
             <li class="nav-item">
                 <a class="nav-link" href="#">Sugerencias</a>
             </li>
+            
+            <form action="index.php" method="post">
+            
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Digitar</a>
                 <ul class="dropdown-menu" aria-labelledby="dropdownId">
 
-                    <li><a class="dropdown-item" href="registros/embolse.php">Action 1</a></li>
-                    <li><a class="dropdown-item" href="registros/embolse.php">Action 2</a></li>
-                
-                
-            </ul>
-        </li>
+                    <li><a class="dropdown-item" type="submit" name="embol" value="embol">Embolse</a></li>
+                    <li><a class="dropdown-item" href="registros/embolse.php">Cosecha</a></li>
+                    <li><a class="dropdown-item" href="registros/embolse.php">Novedades</a></li>
+                    <li><a class="dropdown-item" href="registros/embolse.php">Cosecha</a></li>
+                    
+                    
+                </ul>
+            </li>
+            <input type="submit" value="embol">embolse
+        </form>
         
 </article>
 
@@ -46,59 +53,108 @@
 </article>
 </div>
 </nav>
+
+<?php 
+
+    if (isset($_POST)) {
+        
+        $embolse=$_POST['embol'];
+        include("registros/".$embolse.".php");
+
+
+    }
+
+
+
+?>
+
+
 <div class="container">
 
     <div class="table-responsive">
         <table class="table align-middle table-tam">
             <thead>
-                <tr>
-                    <th scope="col">sem_cosecha</th>
+            <th scope="col">sem_cosecha</th>
                     <th scope="col">fecha</th>
                     <th scope="col">finca</th>
                     <th scope="col">destino</th>
+            </th>
+
+            <tr>
+                <td scope="row"><input type="week" name="" id=""></td>
+                <td scope="row"><input  type="date" name="" id=""></td>
+                <td scope="row"><select name="Finca" value=""id="Finca"></select></td>
+                <td scope="row" class="table-light" id="bloqueo" >exportacion</td>
+            </tr>
+               
+        </thead>
+        <tbody>
+        <tr>
+                    
                     <th scope="col">sem_embolse</th>
                     <th scope="col">cinta</th>
                     <th scope="col">edad</th>
                     <th scope="col">lt_01</th>
                     <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
-                    <th scope="col">lt_02</th>
+                    <th scope="col">lt_03</th>
+                    <th scope="col">lt_04</th>
+                    <th scope="col">lt_05</th>
+                    <th scope="col">lt_06</th>
+                    <th scope="col">lt_07</th>
+                    <th scope="col">lt_08</th>
+                    <th scope="col">lt_09</th>
+                    <th scope="col">lt_10</th>
+                    <th scope="col">lt_12</th>
+                    <th scope="col">lt_13</th>
+                    <th scope="col">lt_14</th>
+                    <th scope="col">lt_15</th>
+                    <th scope="col">lt_16</th>
+                    <th scope="col">lt_17</th>
+                    <th scope="col">lt_18</th>
+                    <th scope="col">lt_19</th>
+                    <th scope="col">lt_20</th>
+                    <th scope="col">lt_21</th>
+                    <th scope="col">lt_22</th>
+                    <th scope="col">lt_23</th>
+                    <th scope="col">lt_24</th>
+                    <th scope="col">lt_25</th>
+                    <th scope="col">lt_26</th>
                     <th scope="col">Total</th>
                 
                 
                 </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td scope="row-xs-2"><input type="week" name="" id=""></td>
-                <td scope="row"><input type="date" name="" id=""></td>
-                <td scope="row"><select name="Finca" id="Finca"></select></td>
-                <td scope="row" class="table-light" id="bloqueo" >exportacion</td>
-                <td><input type="text" name="" ></td>
-                <td><input type="text" name="" ></td>
+           
 
-                <td></td>
+            <tr>
+                <td><input type="text" name="" ></td>
+                <td><input type="color" name="cinta" ></td>
+                <td><input type="text" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
+                <td><input class="lotes" type="number" name="" ></td>
             </tr>
             <tr>
                 <td scope="row"></td>
